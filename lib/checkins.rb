6b5eaa3b0e801629268390a4e46b0c0a4d6d8b17
ronalddijkstra <ronald@defacto.nl>
2019-05-18 10:29:59 +0200

    Use 6 checkins

--- lib/checkins.rb
+++ lib/checkins.rb
@@ -10,7 +10,7 @@ open(url) do |rss|
   feed = RSS::Parser.parse(rss)
 
   File.open("data/checkins.yml", "w") do |f|
-    feed.items[0..6].each do |item|
+    feed.items[0..5].each do |item|
       f.write("- title: \"#{item.title}\"\n")
       f.write("\s\sdescription: \"#{item.description}\"\n")
       f.write("\s\slink: \"#{item.description}\"\n")
