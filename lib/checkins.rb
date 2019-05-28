ce9e348a73cff7596866c36cd3314b40ad360056
ronalddijkstra <ronald@defacto.nl>
2019-05-28 08:40:33 +0200

    Get data

--- lib/checkins.rb
+++ lib/checkins.rb
@@ -1,27 +1,36 @@
 # frozen_string_literal: true
 
-require "rss"
 require "open-uri"
+require "nokogiri"
+require "json"
 
 File.delete("data/checkins.yml") if File.exist?("data/checkins.yml")
 
-url = "https://untappd.com/rss/brewery/371606"
-open(url) do |rss|
-  feed = RSS::Parser.parse(rss)
+doc = Nokogiri::HTML(open("https://untappd.com/Folkingebrew"))
 
-  File.open("data/checkins.yml", "w") do |f|
-    feed.items[0..2].each do |item|
-      f.write("- title: \"#{item.title}\"\n")
-      f.write("\s\sdescription: \"#{item.description}\"\n")
-      f.write("\s\spubdate: \"#{item.pubDate}\"\n")
-      if item.title.include? "Pastry Project #1"
-        f.write("\s\simage: https://labels.untappd.com/labels/3159580?size=hd\"\n")
-      elsif item.title.include? "Back in the Yard DIPA"
-        f.write("\s\simage: https://labels.untappd.com/labels/3050712?size=hd\"\n")
-      else
-        f.write("\s\simage: https://labels.untappd.com/labels/2404100?size=hd\"\n")
-      end
+checkins = doc.css("#main-stream .item")
+
+File.open("data/checkins.yml", "w") do |f|
+  checkins.each do |checkin|
+
+    title = checkin.at(".text").text.strip.gsub(/\s+/, " ")
+    f.write("- title: \"#{title}\"\n")
+
+    if checkin.css("p.photo img").attr("data-original")
+      checkin_image = checkin.css("p.photo img").attr("data-original")
+      f.write("\s\simage: \"#{checkin_image}\"\n")
     end
-    f.close
+
+    if checkin.at("p.comment-text")
+      checkin_comment = checkin.at("p.comment-text").text.strip.gsub(/\s+/, " ")
+      f.write("\s\scomment: \"#{checkin_comment}\"\n")
+    end
+
+    rating = checkin.at(".rating-serving span.rating").attr("class")
+    f.write("\s\srating: \"#{rating}\"\n")
+
+    date_time = checkin.at(".time").text
+    date = Time.parse(date_time)
+    f.write("\s\sdate: \"#{date.day} #{date.strftime("%B")} #{date.year}\"\n")
   end
 end
