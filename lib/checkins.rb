4c1ab6f30c0be7f82c85e1281e24af46b94b2af5
ronalddijkstra <ronald@defacto.nl>
2019-07-26 12:49:44 +0200

    Rescue error

--- lib/checkins.rb
+++ lib/checkins.rb
@@ -4,27 +4,34 @@ require "open-uri"
 require "nokogiri"
 require "json"
 
-File.delete("data/checkins.yml") if File.exist?("data/checkins.yml")
+begin
+  url = "https://untappd.com/Folkingebrew"
+  doc = Nokogiri::HTML(open(url))
+  checkins = doc.css("#main-stream .item")
 
-doc = Nokogiri::HTML(open("https://untappd.com/Folkingebrew"))
+  puts "= Retrieving checkins succeeded, writing checkins.yml"
 
-checkins = doc.css("#main-stream .item")
+  File.open("data/checkins.yml", "w") do |f|
+    checkins.each do |checkin|
+      next unless checkin.css("p.photo img").attr("data-original")
 
-File.open("data/checkins.yml", "w") do |f|
-  checkins.each do |checkin|
-    next unless checkin.css("p.photo img").attr("data-original")
+      user = checkin.at(".text .user").text
+      f.write("- user: \"#{user}\"\n")
 
-    user = checkin.at(".text .user").text
-    f.write("- user: \"#{user}\"\n")
+      title = checkin.at(".text").text.strip.gsub(/\s+/, " ")
+      f.write("\s\stitle: \"#{title}\"\n")
 
-    title = checkin.at(".text").text.strip.gsub(/\s+/, " ")
-    f.write("\s\stitle: \"#{title}\"\n")
+      checkin_image = checkin.css("p.photo img").attr("data-original")
+      f.write("\s\simage: \"#{checkin_image}\"\n")
 
-    checkin_image = checkin.css("p.photo img").attr("data-original")
-    f.write("\s\simage: \"#{checkin_image}\"\n")
-
-    date_time = checkin.at(".time").text
-    date = Time.parse(date_time)
-    f.write("\s\sdate: \"#{date.day}-#{date.month}-#{date.year}\"\n")
+      date_time = checkin.at(".time").text
+      date = Time.parse(date_time)
+      f.write("\s\sdate: \"#{date.day}-#{date.month}-#{date.year}\"\n")
+    end
   end
+
+rescue OpenURI::HTTPError => e
+
+  puts "Retrieving data failed due to #{e}"
+
 end
