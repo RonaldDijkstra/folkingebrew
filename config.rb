ae049ced648964d5579511ba9cc03a2f820607b0
ronalddijkstra <ronald@defacto.nl>
2019-07-31 21:32:20 +0200

    Bundle install

--- config.rb
+++ config.rb
@@ -26,15 +26,7 @@ set :css_dir, "assets/stylesheets"
 set :fonts_dir, "assets/fonts"
 set :images_dir, "assets/images"
 set :js_dir, "assets/javascripts"
-set :markdown,
-  autolink: true,
-  fenced_code_blocks: true,
-  footnotes: true,
-  highlight: true,
-  smartypants: true,
-  strikethrough: true,
-  tables: true,
-  with_toc_data: true
+
 set :markdown_engine, :kramdown
 
 page "/*.json", layout: false
@@ -60,7 +52,7 @@ end
 # Settings for production
 configure :production do
   require "lib/checkins"
-  
+
   activate :asset_hash, ignore: [
     %r{^assets/fonts/.*},
     "assets/images/logo-folkingebrew-black.svg"
