0818e223344da28ce5e06b0339cf0ee37f3f6fea
ronalddijkstra <ronald@defacto.nl>
2019-08-01 17:57:01 +0200

    Use kramdown

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -1,20 +1,7 @@
-module ApplicationHelpers
-  def markdown(contents)
-    renderer = Redcarpet::Render::HTML
-    markdown = Redcarpet::Markdown.new(
-      renderer,
-      autolink: true,
-      fenced_code_blocks: true,
-      footnotes: true,
-      highlight: true,
-      smartypants: true,
-      strikethrough: true,
-      tables: true,
-      with_toc_data: true
-    )
-    markdown.render(contents)
-  end
+# frozen_string_literal: true
 
+## Application Helpers
+module ApplicationHelpers
   # Get the website name from site.yml
   def website_name
     data.site.name
