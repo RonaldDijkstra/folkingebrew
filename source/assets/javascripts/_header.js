9cc2fd6bd5056594b68a5f16c7a4be6160c63443
ronalddijkstra <ronald@defacto.nl>
2020-02-09 22:44:17 +0100

    Fix all page headers

--- source/assets/javascripts/_header.js
+++ source/assets/javascripts/_header.js
@@ -1,3 +1,13 @@
+// Variables
+
+var $navbarHeight = $('.site-header').outerHeight();
+var $didScroll;
+var $lastScrollTop = 0;
+var $delta = 1;
+var $window = $(window);
+
+// Mobile navigation toggle adds nav-open and no-scroll
+
 $(document).ready(function(){
   $('#menu-toggle').on('click', function(){
     $('body').toggleClass('nav-open no-scroll');
@@ -5,24 +15,18 @@ $(document).ready(function(){
   });
 });
 
-$(window).scroll(function() {
-    var navbarHeight = $('.site-header').outerHeight()
-    var scroll = $(window).scrollTop();
+$window.scroll(function() {
+    var $scroll = $window.scrollTop();
 
-    if (scroll >= navbarHeight) {
+    if ($scroll >= $navbarHeight / 10) {
         $(".site-header").addClass("not-at-the-top").removeClass("at-the-top");
     } else {
         $(".site-header").addClass("at-the-top").removeClass("not-at-the-top");
     }
 });
 
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 1;
-var navbarHeight = $('.site-header').outerHeight();
 
-$(window).scroll(function(event){
+$window.scroll(function(event){
     didScroll = true;
 });
 
@@ -34,23 +38,23 @@ setInterval(function() {
 }, 100);
 
 function hasScrolled() {
-    var st = $(this).scrollTop();
+    var $st = $(this).scrollTop();
 
     // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
+    if(Math.abs($lastScrollTop - $st) <= $delta)
         return;
 
     // If they scrolled down and are past the navbar, add class .nav-up.
     // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
+    if ($st > $lastScrollTop && $st > $navbarHeight){
         // Scroll Down
         $('.site-header').removeClass('show-nav').addClass('hide-nav');
     } else {
         // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
+        if($st + $window.height() < $(document).height()) {
             $('.site-header').removeClass('hide-nav').addClass('show-nav');
         }
     }
 
-    lastScrollTop = st;
+    $lastScrollTop = $st;
 }
