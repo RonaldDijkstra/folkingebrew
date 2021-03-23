d46dca5e6f5cb3e89f8f59e94b6cc5c362cef826
ronalddijkstra <ronald@defacto.nl>
2021-03-23 22:38:04 +0100

    Update product page

--- source/assets/javascripts/_webshop.js
+++ source/assets/javascripts/_webshop.js
@@ -1,16 +1,9 @@
 // Update data item quantity with current number
 $(document).ready(function() {
-  $('input.product-quantity-input').change(function(){
-    var $input = $(this).parents('.product-quantity').find('input.product-quantity-input');
+  $('#quantity').change(function(){
     var $button = $(this).parents('.product-controls').find('.buy-button');
 
-    console.log($button);
-
-    $max = $input.attr('max');
-
-    var count = parseFloat($input.val());
-
-    $button.attr('data-item-quantity', count);
+    $button.attr('data-item-quantity', $(this).val());
 
     return false;
   });
