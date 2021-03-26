1f7a0dba8f28eef5fcbc7b70ffe4c40ada6744af
ronalddijkstra <ronald@defacto.nl>
2021-03-26 14:54:12 +0100

    Add cookie for age-consent

---
+++ source/assets/javascripts/_age-gate.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  var ageGate = document.querySelector("[data-rel='age-gate']");
+  var consentButton = document.querySelector("[data-rel='age-gate-consent']");
+  var cookieName = "age_consent";
+  var cookieValue = Cookies.get(cookieName);
+
+  if (!ageGate) {
+    return false;
+  }
+
+  if (cookieValue !== "true") {
+    ageGate.classList.add("show");
+  }
+
+  consentButton.addEventListener("click", closeOverlay);
+
+  function closeOverlay() {
+    ageGate.classList.remove("show");
+
+    Cookies.set(cookieName, true, { expires: 1 });
+  }
+});
--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -3,6 +3,7 @@
 //= require js-cookie
 //= require what-input
 
+//= require _age-gate
 //= require _form
 //= require _header
 //= require _scroll-to
