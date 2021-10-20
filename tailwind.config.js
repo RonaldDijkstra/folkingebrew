d7009711552b6ba9376d1ead2d6958f7db9cc98b
Ronald Dijkstra <ronalddijkstra.nl@gmail.com>
2021-10-20 23:06:22 +0200

    Fix the navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      white: colors.white,
+      black: colors.black,
+      gray: colors.gray,
+      green: {
+        default: "#54b848",
+        dark: "#326f2b",
+      },
+    },
+    extend: {
+      fontFamily: {
+        'open-sans': ['"Open Sans"', 'sans-serif']
+      }
+    }
   },
   variants: {
     extend: {},
