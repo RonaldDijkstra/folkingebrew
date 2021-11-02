9e6caf5ac670dd1e8e6ef29048a8dd3d6b55a157
Ronald Dijkstra <ronalddijkstra.nl@gmail.com>
2021-11-02 07:54:15 +0100

    Add a proper z-index to the nav bar

--- components/beer_card/beer_card_component.rb
+++ components/beer_card/beer_card_component.rb
@@ -9,7 +9,7 @@ module Components
           concat(
             link_to("/beers/#{beer.slug}", class: "bg-gray-800 relative") do
               image_tag(image, alt: beer.title, class: "block w-full", loading: "lazy") +
-              content_tag(:div, class: "opacity-0 w-full h-full bg-black absolute top-0 left-0 z-1 flex flex-col justify-center text-center p-10 hover:opacity-100") do 
+              content_tag(:div, class: "opacity-0 w-full h-full bg-black absolute top-0 left-0 flex flex-col justify-center text-center p-10 hover:opacity-100") do
                 content_tag(:h2, beer.title, class: "text-white text-xl") +
                 content_tag(:div, beer.style, class: "text-green-default")
               end  
