5bec2177c0b8e0df200522e6ae5c97e82a937c70
Ronald Dijkstra <ronalddijkstra.nl@gmail.com>
2021-11-07 13:40:49 +0100

    Print dato env var

--- config.rb
+++ config.rb
@@ -47,7 +47,8 @@ activate :directory_indexes
 activate :inline_svg
 
 # Activate DatoCMS
-activate :dato, preview: ENV["DATO_PREVIEW"], live_reload: true
+activate :dato, preview: ENV['DATO_PREVIEW'], live_reload: true
+
 # Activate Pagination
 activate :pagination
 
