b51e516184591f3c7bfe58150915560742012daa
Ronald Dijkstra <ronalddijkstra.nl@gmail.com>
2021-11-14 14:04:23 +0100

    Make class for regular and markdown content optional

--- components/markdown_content/markdown_content_component.rb
+++ components/markdown_content/markdown_content_component.rb
@@ -2,9 +2,13 @@ module Components
   module MarkdownContent
     class MarkdownContentComponent < Middleman::Extension
       helpers do 
-        def markdown_content(opts, &block)
-          additional_classes = opts.dig(:html, :class) ? " #{opts[:html][:class]}" : ""
-          content_tag(:article, nil, class: "prose max-w-none #{additional_classes}", &block)
+        def markdown_content(opts = nil, &block)
+          if opts 
+            additional_classes = opts.dig(:html, :class) ? " #{opts[:html][:class]}" : ""
+            content_tag(:article, nil, class: "prose max-w-none #{additional_classes}", &block)
+          else 
+            content_tag(:article, nil, class: "prose max-w-none", &block) 
+          end
         end
       end
     end
