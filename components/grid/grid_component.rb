e68cf71326ef28d4102a1c271d08577b4d50e2c4
Ronald Dijkstra <ronalddijkstra.nl@gmail.com>
2021-11-15 16:28:13 +0100

    Fix grid for smaller screens

--- components/grid/grid_component.rb
+++ components/grid/grid_component.rb
@@ -4,7 +4,7 @@ module Components
       helpers do
         def grid(opts, &block)
           additional_classes = opts.dig(:html, :class) ? " #{opts[:html][:class]}" : ""
-          classes = "grid md:grid-cols-2 lg:grid-cols-3 gap-9 mb-10 max-w-7xl m-auto px-6 #{additional_classes}"
+          classes = "grid sm:grid-cols-2 lg:grid-cols-3 gap-9 mb-10 max-w-7xl m-auto px-6 #{additional_classes}"
           return content_tag(opts[:content_tag], class: classes, &block) if opts[:content_tag]
           content_tag(:section, class: classes, &block)
         end
