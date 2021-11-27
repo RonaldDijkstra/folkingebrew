cf06e8f62d899af30608b321ba7bfb1e5bb2bf72
Ronald Dijkstra <ronalddijkstra.nl@gmail.com>
2021-11-27 08:19:04 +0100

    Lint beer card component

--- components/beer_card/beer_card_component.rb
+++ components/beer_card/beer_card_component.rb
@@ -4,18 +4,27 @@ module Components
       helpers do
         def beer_card(opts)
           beer = opts[:beer]
-          image = beer.image.url(fm: :webp, h: 540, w: 540)
 
           concat(
             link_to("/beers/#{beer.slug}/", class: 'bg-gray-800 relative') do
-              image_tag(image, alt: beer.title, class: 'block w-full', loading: 'lazy') +
-              content_tag(:div,
-                          class: 'opacity-0 w-full h-full bg-black absolute top-0 left-0 flex flex-col justify-center text-center p-10 hover:opacity-100') do
+              beer_image(beer) +
+              content_tag(:div, class: classes) do
                 content_tag(:h2, beer.title, class: 'text-white text-xl') +
                 content_tag(:div, beer.style, class: 'text-green-default')
               end
             end
           )
+        end
+
+        private
+
+        def beer_image(beer)
+          image_tag(beer.image.url(fm: :webp, h: 540, w: 540), alt: beer.title, class: 'block w-full', loading: 'lazy')
+        end
+
+        def classes
+          'opacity-0 w-full h-full bg-black absolute top-0 left-0
+           flex flex-col justify-center text-center p-10 hover:opacity-100'
         end
       end
     end
