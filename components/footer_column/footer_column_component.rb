79aa95e0bea3d7411792ea04bda45aef66ceed94
Ronald Dijkstra <ronalddijkstra.nl@gmail.com>
2022-02-07 14:42:54 +0100

    Refactor footer to 5 cols

--- components/footer_column/footer_column_component.rb
+++ components/footer_column/footer_column_component.rb
@@ -3,7 +3,7 @@ module Components
     class FooterColumnComponent < Middleman::Extension
       helpers do
         def footer_column(&block)
-          content_tag(:div, class: 'mb-6 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6', &block)
+          content_tag(:div, class: 'mb-6 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5', &block)
         end
       end
     end
