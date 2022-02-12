61cb9fbc02e4ee503511e70e98b347abc69349be
Ronald Dijkstra <ronalddijkstra.nl@gmail.com>
2022-02-12 08:46:49 +0100

    Refactor nav bar function

--- source/assets/javascripts/header.js
+++ source/assets/javascripts/header.js
@@ -1,16 +1,13 @@
 export default function header() {
-  // Variables
   const navbar = document.querySelector('.site-header');
   const navbarHeight = navbar.offsetHeight;
-  let lastScrollTop = 0;
-  const delta = 1;
   const windowHeight = window.innerHeight;
   const documentBody = document.body;
   const documentHeight = documentBody.scrollHeight;
   const menuToggle = document.getElementById('menu-toggle');
+  let lastScrollTop = 0;
 
   window.addEventListener('DOMContentLoaded', () => {
-    // Mobile navigation toggle
     menuToggle.addEventListener('click', () => {
       documentBody.classList.toggle('mobile-navigation-open');
       documentBody.classList.toggle('overflow-hidden');
@@ -21,26 +18,16 @@ export default function header() {
   document.addEventListener('scroll', () => {
     const scroll = window.scrollY;
 
-    // Add and remove top classes
     if (scroll >= navbarHeight / 10) {
-      documentBody.classList.add('not-at-the-top');
       documentBody.classList.remove('at-the-top');
     } else {
       documentBody.classList.add('at-the-top');
-      documentBody.classList.remove('not-at-the-top');
     }
-
-    // Show and hide navigation bar
-    if (Math.abs(lastScrollTop - scroll) <= delta) return;
 
     if (scroll > lastScrollTop && scroll > navbarHeight) {
-      navbar.classList.remove('opacity-100');
-      navbar.classList.add('opacity-0');
+      navbar.classList.replace('opacity-100', 'opacity-0');
     } else if (scroll + windowHeight < documentHeight) {
-      navbar.classList.remove('opacity-0');
-      navbar.classList.add('opacity-100');
+      navbar.classList.replace('opacity-0', 'opacity-100');
     }
-
-    lastScrollTop = scroll;
   });
 }
