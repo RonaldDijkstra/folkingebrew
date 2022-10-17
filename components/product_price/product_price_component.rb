7d89c99b113233d18736caa9a7498712c92bf648
Ronald Dijkstra <ronalddijkstra.nl@gmail.com>
2022-10-17 23:23:42 +0200

    Even more cms (#579)
    * Get contact meta from dato
    
    * Remove white lines
    
    * Add price classes
    
    * Fix about image caption

--- components/product_price/product_price_component.rb
+++ components/product_price/product_price_component.rb
@@ -2,8 +2,8 @@ module Components
   module ProductPrice
     class ProductPriceComponent < Middleman::Extension
       helpers do
-        def product_price(product)
-          return old_price(product) + current_price(product) if product.old_price
+        def product_price(product, classes = nil)
+          return old_price(product, classes) + current_price(product) if product.old_price
 
           current_price(product)
         end
@@ -13,9 +13,9 @@ module Components
                       class: 'text-2xl font-bold whitespace-nowrap text-green-default')
         end
 
-        def old_price(product)
+        def old_price(product, classes)
           content_tag(:div, "#{price(product.old_price)}",
-                      class: 'line-through whitespace-nowrap text-gray-400')
+                      class: "#{classes} line-through whitespace-nowrap text-gray-400")
         end
 
         def price(price)
