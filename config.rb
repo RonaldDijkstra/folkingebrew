71bb4a91dddf659281fa4696f714a12ceec8fc86
Ronald Dijkstra <ronald.dijkstra@voys.nl>
2025-05-19 09:09:26 +0200

    fix: Register webshop

--- config.rb
+++ config.rb
@@ -106,25 +106,6 @@ ready do
   proxy '_redirects', 'redirects', ignore: true
 end
 
-dato.tap do |dato|
-  # paginate dato.beers, '/beers', '/templates/beers.html', per_page: 12
-  paginate dato.products, '/webshop', '/templates/webshop.html'
-
-  # dato.beers.each do |beer|
-  #   proxy "/beers/#{beer.slug}/index.html",
-  #         '/templates/beer.html',
-  #         locals: { beer: beer },
-  #         ignore: true
-  # end
-
-  dato.products.each do |product|
-    proxy "/webshop/#{product.slug}/index.html",
-          '/templates/product.html',
-          locals: { product: product },
-          ignore: true
-  end
-end
-
 ignore '/templates/beers.html.erb'
 ignore '/templates/webshop.html.erb'
 
@@ -150,6 +131,14 @@ activate :blog do |blog|
   blog.filter = ->(article) { article.path.match(/\/(\d+)-/)[1].to_i }
 end
 
+activate :blog do |blog|
+  blog.name = "webshop"
+  blog.prefix = false
+  blog.sources = "/webshop/products/{id}-{title}.html"
+  blog.permalink = "/webshop/{title}"
+  blog.paginate = false
+end
+
 # With no layout
 page "/*.xml", layout: false
 page "/*.json", layout: false
@@ -162,5 +151,9 @@ page "blog/feed.xml", layout: false
 
 page "beers/index.html", layout: :beers_layout
 page "beers/*", layout: :beer_layout
+
+page "webshop/index.html", layout: :products_layout
+page "webshop/*", layout: :product_layout
+
 
 
---
+++ source/webshop/products/127-zipped-hoodie-folkingebrew.html.markdown
@@ -0,0 +1,26 @@
+---
+title: "Zipped Hoodie Folkingebrew"
+date: "01-01-2025"
+product_id: 127
+in_stock: true
+only_a_few_left: false
+images:
+  - "hoodie-zipped-front.webp"
+  - "hoodie-zipped-back.webp"
+price: 
+old_price: 
+deposit:
+show_size_information: true
+sizes:
+  - name: "S"
+    size_in_stock: true
+    lxb: "67 x 51"
+  - name: "M"
+    size_in_stock: true
+    lxb: "70 x 56"
+size_image:
+short_description: 
+product_details: "52% Cotton - 48% Polyester"
+---
+
+Charcoal Zipped Hoodie with small black Folkingebrew logo on the front and our full logo on the back.
