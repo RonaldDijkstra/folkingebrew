5207a1534c6fb30ceee81a9055511a1e78c574a2
Ronald Dijkstra <ronald.dijkstra@voys.nl>
2025-08-21 16:51:35 +0200

    feat(theme): Add logo

--- web/app/themes/folkingebrew/resources/views/sections/header.blade.php
+++ web/app/themes/folkingebrew/resources/views/sections/header.blade.php
@@ -1,11 +1,14 @@
 <header class="banner bg-black px-2 md:px-6 h-16 md:h-20 flex items-center justify-between">
-  <a class="brand inline-block md:w-15 md:shrink-0" href="{{ home_url('/') }}">
-    <img src="{{ Vite::asset('resources/images/logo-folkingebrew-hop.svg') }}" alt="{{ $siteName }}" class="h-12 w-auto">
-  </a>
-  <div class="flex-1 text-white text-center">
+  <div class="flex items-center w-full justify-center md:w-auto">
+    <a class="brand inline-block md:w-15 md:shrink-0" href="{{ home_url('/') }}">
+      <img src="{{ Vite::asset('resources/images/logo-folkingebrew-hop.svg') }}" alt="{{ $siteName }}" class="h-12 w-auto">
+    </a>
+  </div>
+
+  <div class="hidden md:block flex-1 text-white text-center">
     <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
       @foreach ($primaryNavigation as $item)
-        <a href="{{ $item->url }}" class="text-white no-underline hover:underline hover:text-primary px-4 text-xl">{{ $item->label }}</a>
+        <a href="{{ $item->url }}" class="text-white no-underline hover:underline hover:text-primary px-3 text-lg">{{ $item->label }}</a>
       @endforeach
     </nav>
   </div>
