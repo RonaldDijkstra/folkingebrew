2121ba0f283f84ea3562df67bd4bf72476a41d1a
Ronald Dijkstra <ronald.dijkstra@voys.nl>
2025-08-21 21:18:31 +0200

    feat: Add fonts

--- web/app/themes/folkingebrew/app/filters.php
+++ web/app/themes/folkingebrew/app/filters.php
@@ -14,3 +14,14 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+/**
+ * Add font-sans class to body.
+ *
+ * @param array $classes
+ * @return array
+ */
+add_filter('body_class', function ($classes) {
+    $classes[] = 'font-sans';
+    return $classes;
+});
--- web/app/themes/folkingebrew/resources/views/sections/header.blade.php
+++ web/app/themes/folkingebrew/resources/views/sections/header.blade.php
@@ -1,14 +1,13 @@
-<header class="banner bg-black px-2 md:px-6 h-16 md:h-20 flex items-center justify-between">
+<header class="bg-black px-2 md:px-6 h-16 md:h-20 flex items-center justify-between">
   <div class="flex items-center w-full justify-center md:w-auto">
-    <a class="brand inline-block md:w-15 md:shrink-0" href="{{ home_url('/') }}">
+    <a class="inline-block md:w-15 md:shrink-0" href="{{ home_url('/') }}">
       <img src="{{ Vite::asset('resources/images/logo-folkingebrew-hop.svg') }}" alt="{{ $siteName }}" class="h-12 w-auto">
     </a>
   </div>
-
   <div class="hidden md:block flex-1 text-white text-center">
     <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
       @foreach ($primaryNavigation as $item)
-        <a href="{{ $item->url }}" class="text-white no-underline hover:underline hover:text-primary px-3 text-lg">{{ $item->label }}</a>
+        <a href="{{ $item->url }}" class="text-white no-underline hover:underline hover:text-primary px-2 text-md">{{ $item->label }}</a>
       @endforeach
     </nav>
   </div>
