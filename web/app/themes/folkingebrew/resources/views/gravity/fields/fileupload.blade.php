89b981915ee369c20f35a2999129eaf17e09bdc1
Ronald Dijkstra <ronald.dijkstra@voys.nl>
2025-10-07 21:44:59 +0200

    feat(forms): Style single file upload

--- web/app/themes/folkingebrew/resources/views/gravity/fields/fileupload.blade.php
+++ web/app/themes/folkingebrew/resources/views/gravity/fields/fileupload.blade.php
@@ -12,8 +12,11 @@
     type="file"
     id="{{ $inputId }}"
     name="{{ $inputName }}"
-    @if(!empty($isRequired)) required @endif
-    @if($accept !== '*') accept="{{ $accept }}" @endif
+    @if($isRequired) aria-required="true" required @endif
+    @if($failed) aria-invalid="true" @endif
+    aria-describedby="{{ $ariaDescId }}"
+    class="block w-full text-sm text-body file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-medium file:bg-primary/20 file:text-primary hover:file:bg-primary/30 border rounded @if($failed) border-red-500 @else border-gray-300 @endif"
+    @if(isset($field) && property_exists($field, 'allowedExtensions') && $field->allowedExtensions) accept="{{ $field->allowedExtensions }}" @endif
   />
   @if(!empty($maxFileSize))
     <div>{{ __("Max file size", "folkingebrew") }}: {{ $maxFileSizeMB }} MB</div>
